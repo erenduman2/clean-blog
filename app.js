9a25efe11c208d6f8d7588552d449f364038ef29
erenduman2 <71399989+erenduman2@users.noreply.github.com>
2022-09-12 15:06:46 +0300

    odev-2

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-const express = require("express");
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
-  res.send(blog);
+app.set('view engine', 'ejs');
+
+// MIDDLEWARE
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log("sunucu basladi");
+  console.log('sunucu basladi');
 });
